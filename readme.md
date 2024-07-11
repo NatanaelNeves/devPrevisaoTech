78df4c3300f25cb434c52874335128cc8cff3e24
NatanaelNeves <natanaelnevesalves@gmail.com>
2024-07-11 16:53:48 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,23 @@
+<h1 align="center"> Dev Previsão </h1>
+
+<div align="center">
+
+<img src="./assets/devprevisao3.jpeg" alt=""> 
+
+<h3 align="center"> <b>📱 Responsivo</b></h3>
+<img src="./assets/devprevisao2.jpeg" alt=""> 
+
+</div>
+
+## 🚀 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- HTML e CSS
+- JavaScript
+- Axios
+
+## 💻 Projeto
+
+Aplicação de previsão do tempo, integrando uma API que retorna dados meteorológicos atualizados em tempo real. Essa aplicação permitiu que os usuários obtivessem informações precisas sobre o clima, auxiliando no planejamento de suas atividades diárias e agregando valor ao fornecer insights úteis para tomadas de decisão.
+
---
+++ script.js
@@ -0,0 +1,40 @@
+let key = "5414542d9e0a55b4a6e025d9e5c36fb6";
+
+function putOnScreen(data) {
+  console.log(data)
+  document.querySelector(".city").innerHTML = data.name;
+  document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + "°C";
+  document.querySelector(".description").innerHTML =data.weather[0].description;
+  document.querySelector(".icon").src ="https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
+  document.querySelector(".temp-max").innerHTML ="Temp-max: " + Math.floor (data.main.temp_max) + "°C";
+  document.querySelector(".sensation").innerHTML ="Sensação: " + Math.floor (data.main.feels_like) + "°C";
+  document.querySelector(".umidity").innerHTML ="Umidade: " + data.main.humidity + "%";
+  document.querySelector(".temp-min").innerHTML ="Temp-min: " + Math.floor (data.main.temp_min) + "°C";
+}
+
+async function seekCity(city) {
+  let data = await fetch(
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      key +
+      "&lang=pt_br" +
+      "&units=metric"
+  ).then((response) => response.json());
+
+  putOnScreen(data);
+}
+
+function clickButton() {
+  let city = document.querySelector(".input-city").value;
+
+  seekCity(city);
+}
+
+
+
+
+
+
+
+
